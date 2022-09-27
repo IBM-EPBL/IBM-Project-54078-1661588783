c330fc5647297d9862d46a0001cf1bf9d426102d
Somnath R <82262609+Githubb-42@users.noreply.github.com>
2022-09-27 21:37:27 +0530

    Create app.py

---
+++ EXPENSE_TRACKER/TM3-SOMANATH_DAS/ASSIGNMENT/ASSIGNMENT-2/Flask_program/app.py
@@ -0,0 +1,43 @@
+from contextlib import redirect_stderr
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+app=Flask(__name__)
+app.config['SECRET_KEY']='df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506'
+
+messages = [{'title': 'Message One',
+            'content': 'Message One Content' },
+            {'title': 'Message Two',
+            'content': 'Message Two Content'}]
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+      if request.method== 'POST':
+            title= request.form['title']    
+            content= request.form['content']
+            
+            if not title:
+                  flash('Title is required!')
+            elif not content:
+                  flash('Content is required!')
+            else:
+                  messages.append({'title': title, 'content': content})
+                  name = "Shyam Mohan"
+                  return redirect(url_for('index', messages=name ))
+
+      return render_template('create.html')
+
+
+@app.route('/')
+def index():
+      return render_template('index.html',messages=messages)
+
+@app.route('/admin')
+def hello_admin():
+      return 'Hello admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+      return'Hello %s as Guest'%guest
+
+
+
